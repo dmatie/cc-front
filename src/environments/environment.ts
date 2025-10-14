85cffb5993c2498e2ad5625f30f51e6571adf43e
Matié DIARRASSOUBA <m.diarrassouba@afdb.org>
2025-10-14 02:34:19 +0000

    maj env param

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     azureAd: {
         clientId: '7b937ab1-2b96-4206-b373-f0aacb79b4a7', // Client ID de test - À remplacer
         authority: 'https://login.microsoftonline.com/common', // Utilise 'common' pour test
-        redirectUri: https://cc-perso-app.azurewebsites.net/client/home',
+        redirectUri: 'https://cc-perso-app.azurewebsites.net/client/home',
         postLogoutRedirectUri: 'https://cc-perso-app.azurewebsites.net/home',
         backendScope:'api://586ffe0a-59c4-4e52-af81-be102e2e6e07/access_as_user'
     },
